624a9592a7ba0757675fe97b0bec420e50a81f78
Blackhalo3k <blackhalo3k.9@gmail.com>
2026-06-30 14:21:23 -0400

    added info

---
+++ raspberry-pi-foundations/infocard.py
@@ -0,0 +1,13 @@
+name = "Brian Odom"
+age = "16, going to 11th Grade"
+
+interests = "Gaming, Reading, Music, Drawing"
+reason = "I joined because I want to find more information about things I enjoy(Computing)"
+goals = "I want to learn as much as I can while im here."
+personality = "Im an Introverted, Open minded, Nerdy, Creative, Competitive, and Passionate"
+
+print("Name: ", name)
+print("Age/Grade: ", age)
+print("\nInterests: ", interests)
+print("Why I'm Here: ", goals)
+print("Personality: ", personality)
