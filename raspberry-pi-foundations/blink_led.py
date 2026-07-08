149d818ea0333e51affd2af3f7580b8d448ef138
Blackhalo3k <blackhalo3k.9@gmail.com>
2026-07-08 10:58:59 -0400

    blink led sketch

---
+++ raspberry-pi-foundations/blink_led.py
@@ -0,0 +1,13 @@
+#SImple blink LED Sketch Using GPIO pin on Raspberry Pi
+
+from gpiozero import LED
+from time import sleep 
+
+led = LED(17)
+
+while True:
+    led.on()
+    sleep(1)
+    led.off()
+    sleep(1)
+    
